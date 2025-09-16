7420c4d080638708395ec6cf90b2146752437597
MD Jahangir Alam <labibpop120@gmail.com>
2025-09-16 00:08:56 +0000

    chenge port

--- aide.py
+++ aide.py
@@ -244,7 +244,7 @@ def git_clone():
 # ==================== Run App ====================
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
 
 
 
